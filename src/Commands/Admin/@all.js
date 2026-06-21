59e10fde0bed71bd55e6bf4eb00e6a98cfd0e810
CRYSTAL-LEVI UZOUKWU <crysnovax@gmail.com>
2026-06-21 11:29:14 +0100

    🏷️

---
+++ src/Commands/Admin/@all.js
@@ -0,0 +1,21 @@
+// ── tagall.js ────────────────────────────────────────────────────
+module.exports = {
+    name: 'all',
+    alias: [],
+    desc: 'Mention all members in the group',
+    category: 'Group',
+    admin: false,
+    group: true,
+    usage: 'all',
+
+    execute: async (sock, m, { reply }) => {
+        try {
+            await sock.sendMessage(m.chat, {
+                text: '@all',
+                mentionAll: true
+            }, { quoted: m });
+        } catch (err) {
+            reply(`⊘ ${err.message}`);
+        }
+    }
+};
